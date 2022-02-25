bd3ca425f3e8917ea8848f253d46a9f51600afa9
BrantLauro <brantlauro@gmail.com>
2022-02-25 14:14:44 -0300

    enemy spawn and player death

--- scripts/arena.gd
+++ scripts/arena.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
+var enemy_1 = preload("res://scenes/enemy.tscn")
+
 func _ready():
 	Global.child_node_creation = self
 
 func _exit_tree():
 	Global.child_node_creation = null
+
+
+func _on_TimerEnemySpawn_timeout():
+	var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y > -45:
+		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
+	Global.instance_node(enemy_1, enemy_position, self)
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ var speed = 150
 var mov = Vector2.ZERO
 var shoot = preload("res://scenes/shoot.tscn")
 var reloaded = true
+var dead = false
 
 func _ready():
 	Global.player = self
@@ -14,12 +15,20 @@ func _exit_tree():
 func _process(delta: float):
 	mov.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	mov.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
-	global_position += speed * mov * delta
+	if dead == false:
+		global_position += speed * mov * delta
 	
-	if Input.is_action_pressed("shoot") and Global.child_node_creation != null and reloaded:
+	if Input.is_action_pressed("shoot") and Global.child_node_creation != null and reloaded and dead == false:
 		Global.instance_node(shoot, global_position, Global.child_node_creation)
 		reloaded = false
 		$ReloadTimer.start()
 
 func _on_ReloadTimer_timeout():
 	reloaded = true
+
+func _on_HitBox_area_entered(area: Area2D):
+	if area.is_in_group("enemy"):
+		visible = false
+		dead = true
+		yield(get_tree().create_timer(1), "timeout")
+		get_tree().reload_current_scene()
